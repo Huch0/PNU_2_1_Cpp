213530eea66633c6c7a1a710db260348c21926ea
huch0 <heochiyeong@gmail.com>
2023-04-15 17:52:13 +0900

    6th assignment

--- 04_13/6장과제2023.cpp
+++ 04_13/6장과제2023.cpp
@@ -8,6 +8,9 @@ class String {
     char* data;
 
 public:
+    String() {
+        this->data = new char[1];
+    }
     String(char* data) {
         this->data = new char[strlen(data) + 1];
         strcpy(this->data, data);
@@ -23,6 +26,7 @@ public:
 
     String& operator=(String& other);
     int operator>(String& other);
+    int operator==(String& other);
 
     friend ostream& operator<<(ostream& stream, const String& s);
 };
@@ -38,11 +42,20 @@ String& String::operator=(String& other) {
 // strcmp : 아스키 코드가 더 큰지 확인 > 알파벳 역순. b > a
 // 리턴 1 : 왼쪽이 오른쪽보다 나중 순서임.
 int String::operator>(String& other) {
-    if (strcmp(this->data, other.data))
+    if (strcmp(this->data, other.data) > 0) 
         return 1;
-    else
+    else {
         return 0;
+    }
 }
+int String::operator==(String& other) {
+    if (strcmp(this->data, other.data) == 0) 
+        return 1;
+    else {
+        return 0;
+    }
+}
+
 ostream& operator<<(ostream& stream, const String& s) {
     for (int i = 0; s.data[i] != '\0'; i++) {
         stream << s.data[i];
@@ -55,11 +68,11 @@ class Person {
     int age;
 public:
     Person() {
-        this->pname = nullptr;
+        this->pname = new String();
         this->age = 0;
     }
     Person(int age) {
-        this->pname = nullptr;
+        this->pname = new String();
         this->age = age;
     }
     Person(char* pname) {
@@ -74,11 +87,16 @@ public:
         this->pname = new String(pp->pname);
         this->age = pp->age;
     }
+    Person(Person& pr) {
+        this->pname = new String(pr.pname);
+        this->age = pr.age;
+    }
     ~Person() {
         delete this->pname;
     }
 
     Person& operator=(Person& other);
+    Person& operator=(Person* other);
     int operator>(Person& other);
 
     friend ostream& operator<<(ostream& stream, const Person& p);
@@ -91,12 +109,24 @@ Person& Person::operator=(Person& other) {
     
     return *this;
 }
+Person& Person::operator=(Person* other) {
+    delete this->pname;
+    this->pname = new String(other->pname);
+    this->age = other->age;
+
+    return *this;
+}
 
 int Person::operator>(Person& other) {
-    if (this->pname > other.pname)
+    if (*this->pname > *other.pname) {
         return 1;
-    else
+    } else if (*this->pname == *other.pname) {
+        if (this->age > other.age) {
+            return 1;
+        }    
+    } else {
         return 0;
+    }
 }
 ostream& operator<<(ostream& stream, const Person& p) {
     stream << "name : " << *(p.pname);
@@ -124,15 +154,19 @@ public:
     int Push(Person& p);
     Person* Pop();
     int getLength();
+    void show();
     friend ostream& operator<<(ostream& stream, const Stack& stack);
+    Stack& operator=(Stack& other);
 };
 int Stack::Push(Person& p) {
     if (this->top == 100) {
         cout << "Stack is full" << endl;
         return 0;
     }
-    this->items[top] = &p;
-    top += 1;
+    cout << "top : " << this->top << "Person : " << p << endl;
+    Person* tmp = new Person(p);
+    this->items[this->top] = tmp;
+    this->top += 1;
 
     return 1;
 }
@@ -141,7 +175,7 @@ Person* Stack::Pop() {
         cout << "Stack is empty" << endl;
         return nullptr;
     }
-    top -= 1;
+    this->top -= 1;
     Person* result = this->items[top];
     this->items[top] = nullptr;
 
@@ -150,6 +184,14 @@ Person* Stack::Pop() {
 int Stack::getLength() {
     return this->top;
 }
+
+void Stack::show() {
+    cout << "stack show : " << endl;
+    for (int i = 0; i < this->top; i++) {
+        cout << *(this->items[i]) << endl;
+    }
+    return;
+}
 ostream& operator<<(ostream& stream, const Stack& stack) {
     for (int i = 0; i < stack.top; i++) {
         stream << *(stack.items[i]) << endl;
@@ -157,13 +199,29 @@ ostream& operator<<(ostream& stream, const Stack& stack) {
 
     return stream;
 }
+Stack& Stack::operator=(Stack& other){
+    if (this != &other) {
+        for (int i = 0; i < this->top; i++) {
+            delete this->items[i];
+            this->items[i] = nullptr;
+        }
+        for (int i = 0; i < other.top; i++) {
+            Person* tmp = new Person(other.items[i]);
+            this->items[i] = tmp;
+        }
+        this->top = other.top;
+    }
+
+    return *this;
+}
 
 int main() {
     Stack s1;
+    Stack s2;
+    Person* p[10];
     while (1)
     {
-        Stack s2;
-        char* sname;
+        char sname[20];
         int year;
 
         int select;
@@ -177,7 +235,7 @@ int main() {
                 Person p1("kim");
                 Person p2(23);
                 Person p3("park", 24);
-                Person* p[10];
+                
                 p[0] = &p0; p[1] = &p1; p[2] = &p2; p[3] = &p3;
                 for (int i = 0; i < 4; i++) {
                     s2.Push(*p[i]);
@@ -188,18 +246,22 @@ int main() {
                 cout << endl << "1개 객체를 화면 입력받아" << endl;
 
                 cin >> sname >> year;
-                Person px = Person(sname, year);
+                Person px(sname, year);
                 s2.Push(px);
                 break;
             }
             case 3: {//Delete
                 cout << endl << "Delete" << endl;
-                Person result = s2.Pop();
-                cout << "삭제된 객체:";
-                cout << result;
+                Person* result_ptr = s2.Pop();
+                if (result_ptr != nullptr) {
+                    Person result = *result_ptr;
+                    cout << "삭제된 객체:";
+                    cout << result;
+                }
                 break;
             }
             case 4: {//Stack에 있는 모든 객체를 출력
+                //cout << s2.getLength() << endl;
                 cout << s2;
                 break;
             }
@@ -212,12 +274,25 @@ int main() {
                 //arry[] 객체를 person의 이름으로 정렬하기 > person의 비교 연산자 구현
                 //정렬된 객체를 출력하기
                 for (int i = 0; i < stack_length; i++) {
-                    cout << *arry[i];
+                    for (int j = 0; j < stack_length - i - 1; j++) {
+                        if (*(arry[j]) > *(arry[j + 1])) {
+                            Person* tmp = arry[j];
+                            arry[j] = arry[j + 1];
+                            arry[j + 1] = tmp;
+                        }
+                    }
+                }
+                for (int i = 0; i < stack_length; i++) {
+                    cout << *arry[i] << endl;
                 }
                 break;
             }
             case 6: {
                 s1 = s2; //s2의 모든 객체를 s1에 넣음. s2는 날아가도 s1의 객체들은 남게 
+                s1.show();
+                s2.~Stack();
+                cout << "after s2 deleted" << endl;
+                s1.show();
                 break;
             }
             default: {
